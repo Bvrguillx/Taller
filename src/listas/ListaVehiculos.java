7655475a599b9e68e3f19aa93dcb86fb5656c038
Triboga <Triboga@172.16.41.1>
2017-05-24 12:08:58 +0200

    BVrgui mariconson

---
+++ src/listas/ListaVehiculos.java
@@ -0,0 +1,9 @@
+package listas;
+
+import java.util.ArrayList;
+
+public class ListaVehiculos {
+
+	@SuppressWarnings("rawtypes")
+	protected ArrayList listaVehiculo = new ArrayList ();
+}
--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,5 +1,22 @@
 package models;
 
 public class Cliente {
+	
+	protected String nombre;
+	protected int cp;
+	protected String apellido;
+	protected String dni;
+	protected float telefono;
+	
+	public Cliente(String nombre, int cp, String apellido, String dni, float telefono) {
 
+		this.nombre = nombre;
+		this.cp = cp;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.telefono = telefono;
+	}
+	
+	
+	
 }
