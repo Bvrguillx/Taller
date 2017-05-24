e1d3f8d1705fff77ea10bd98ab4f3ae0f16983c8
admincenec <admincenec@AULA519.cenecmalaga.net>
2017-05-24 08:52:06 +0200

    Añadida validacion con expresiones regulares al textfield de matricula.

--- src/ventanas/BuscarVehiculo.java
+++ src/ventanas/BuscarVehiculo.java
@@ -4,17 +4,24 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class BuscarVehiculo {
 
 	private JFrame frameBuscarMatricula;
 	private JTextField textField;
+	private JButton btnOk;
+	private JButton btnBack;
+	private JLabel lblMatricula;
+	private JLabel lblIntroducirMatricula;
 
 	/**
 	 * Launch the application.
@@ -42,66 +49,75 @@ public class BuscarVehiculo {
 	 */
 	public BuscarVehiculo() {
 		initialize();
+		setEventos();
+		setPropiedades();
+
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
+		// frame
 		frameBuscarMatricula = new JFrame();
+
+		// label Titulo
+
+		lblMatricula = new JLabel("Introduzca Matr\u00EDcula");
+
+		// Label matricula
+		lblIntroducirMatricula = new JLabel("Matr\u00EDcula :");
+
+		// botn ok
+		btnOk = new JButton("OK");
+
+		// campo texto
+		textField = new JTextField();
+
+		// boton atras
+		btnBack = new JButton("");
+
+	}
+
+	private void setPropiedades() {
+		
+		//Propiedades frame
 		frameBuscarMatricula.setTitle("Busqueda Matr\u00EDcula");
 		frameBuscarMatricula.setBounds(100, 100, 450, 300);
 		frameBuscarMatricula.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameBuscarMatricula.getContentPane().setLayout(null);
 
-		JLabel lblMatricula = new JLabel("Introduzca Matr\u00EDcula");
+		//propieades Label Titulo
 		lblMatricula.setFont(new Font("Tahoma", Font.PLAIN, 26));
 		lblMatricula.setBounds(94, 0, 269, 43);
 		frameBuscarMatricula.getContentPane().add(lblMatricula);
 
-		textField = new JTextField();
+		//Propiedades texto
 		textField.setBounds(164, 61, 170, 43);
 		frameBuscarMatricula.getContentPane().add(textField);
 		textField.setColumns(10);
 
-		JLabel lblIntroducirMatricula = new JLabel("Matr\u00EDcula :");
+		//Propiedades label matricula
 		lblIntroducirMatricula.setFont(new Font("Tahoma", Font.PLAIN, 21));
 		lblIntroducirMatricula.setBounds(55, 73, 99, 23);
 		frameBuscarMatricula.getContentPane().add(lblIntroducirMatricula);
 
-		JButton btnOk = new JButton("OK");
-		btnOk.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				// añadir condicion de si existe o no existe matricula y enlazar
-				// a
-				// ventana.
-				// si existe
-				/*
-				 * FichaVehiculoCrear ventanaVehiculo = new
-				 * FichaVehiculoCrear();
-				 * 
-				 * ventanaVehiculo.getFrameCrearFichaVehiculo().setVisible(true)
-				 * ;
-				 * 
-				 * frameDni.dispose();
-				 */
-				// si no existe
-
-				FichaReparar ventanaReparar = new FichaReparar();
-
-				ventanaReparar.getframeFichaReparar().setVisible(true);
-
-				frameBuscarMatricula.dispose();
-
-			}
-		});
+		//Propiedades Boton ok
 		btnOk.setFont(new Font("Tahoma", Font.PLAIN, 60));
 		btnOk.setBounds(94, 136, 114, 98);
 		frameBuscarMatricula.getContentPane().add(btnOk);
 
-		JButton btnBack = new JButton("");
+		//Propiedades boton back
 		btnBack.setFont(new Font("Tahoma", Font.PLAIN, 44));
+		btnBack.setIcon(
+				new ImageIcon(BuscarVehiculo.class.getResource("/com/sun/javafx/scene/web/skin/Undo_16x16_JFX.png")));
+		btnBack.setBounds(249, 136, 114, 98);
+		frameBuscarMatricula.getContentPane().add(btnBack);
+	}
+
+	private void setEventos() {
+		// Eventos boton atras hacia principal
 		btnBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -112,9 +128,50 @@ public class BuscarVehiculo {
 			}
 
 		});
-		btnBack.setIcon(
-				new ImageIcon(BuscarVehiculo.class.getResource("/com/sun/javafx/scene/web/skin/Undo_16x16_JFX.png")));
-		btnBack.setBounds(249, 136, 114, 98);
-		frameBuscarMatricula.getContentPane().add(btnBack);
+
+		// Eventos boton Matricula introducida hacia destino
+
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+
+				//4 numeros, 3 letras al final
+				Pattern tresLetrasFinal = Pattern.compile("^\\d{4}[A-Z]{3}");
+				Matcher matricula3letras = tresLetrasFinal.matcher(textField.getText().toUpperCase().replaceAll(" ", ""));	
+				
+				//1 o2 letras, 4 numeros, 2 letras
+				Pattern cuatroNumerosCentro = Pattern.compile("^\\d{1,2}[A-Z]{4}\\d{2}");
+				Matcher matricula4numeros = cuatroNumerosCentro.matcher(textField.getText().toUpperCase().replaceAll(" ", ""));	
+				
+				if(matricula3letras.matches()||matricula4numeros.matches()){
+					JOptionPane.showMessageDialog(null, "Matricula correcta");
+				}
+				else{
+					JOptionPane.showMessageDialog(null, "Matricula incorrecta");
+
+				}
+						
+				
+			
+				// si NO existe la matricula
+				/*
+				 * FichaVehiculoCrear ventanaVehiculo = new FichaVehiculoCrear();
+				 * 
+				 * ventanaVehiculo.getFrameCrearFichaVehiculo().setVisible(true);
+				 * 
+				 * frameBuscarMatricula.dispose();
+				 
+				// Si existe la matricula
+
+				FichaReparar ventanaReparar = new FichaReparar();
+
+				ventanaReparar.getframeFichaReparar().setVisible(true);
+
+				frameBuscarMatricula.dispose();
+*/
+			}
+		});
+
 	}
+
 }
