fa9b8a0f1114e399d53b783dc356869bb04e0225
Triboga <Triboga@192.168.0.105>
2017-05-27 21:46:54 +0200

    menuses en vehiculo

--- src/ventanas/FichaVehiculoCrear.java
+++ src/ventanas/FichaVehiculoCrear.java
@@ -55,6 +55,22 @@ public class FichaVehiculoCrear {
 	private JButton btnAtras_1;
 	private int indiceVehiculos;
 
+	// MENU
+	private JMenu mnMenu;
+	private JMenu mnCoches;
+	private JMenuItem mntmCocheLeer;
+	private JMenuItem mntmCocheEditar;
+	private JMenuItem mntmCocheCrear;
+	private JMenu mnClientes;
+	private JMenuItem mntmClienteLeer;
+	private JMenuItem mntmClienteCrear;
+	private JMenuItem mntmReparacionCrear;
+	private JMenuItem mntmReparacionEditar;
+	private JMenuItem mntmReparacionLeer;
+	private JMenuItem mntmClienteEditar;
+	private JMenu mnReparaciones;
+	private JMenuBar menuBar;
+
 	// ATRIBUTOS VEHICULO
 	protected String matricula_;
 	protected int km_;
@@ -63,7 +79,6 @@ public class FichaVehiculoCrear {
 	protected String color_;
 	protected int potencia_;
 	protected JTextField clienteField;
-	private JMenu mnReparaciones;
 	// protected String dniCliente_;
 
 	/**
@@ -128,6 +143,18 @@ public class FichaVehiculoCrear {
 		lblUsuario = new JLabel("Mecanico");
 		btnAtras_1 = new JButton("");
 		btnAlante = new JButton("");
+		//MENU
+		mnMenu = new JMenu("");
+		mnCoches = new JMenu("");
+		mntmCocheLeer = new JMenuItem("");
+		mntmCocheEditar = new JMenuItem("");
+		mntmCocheCrear = new JMenuItem("");
+		mnClientes = new JMenu("");
+		mntmClienteLeer = new JMenuItem("");
+		mntmClienteCrear = new JMenuItem("");
+		mntmReparacionCrear = new JMenuItem("");
+		mntmReparacionEditar = new JMenuItem("");
+		mntmReparacionLeer = new JMenuItem("");
 
 	}
 
@@ -266,23 +293,56 @@ public class FichaVehiculoCrear {
 				.getResource("/com/sun/javafx/scene/control/skin/modena/HTMLEditor-Outdent-Black-rtl.png")));
 		btnAlante.setBounds(543, 53, 65, 53);
 		frameCrearFichaVehiculo.getContentPane().add(btnAlante);
-		
-		JMenu mnMenu = new JMenu("");
-		mnMenu.setBounds(0, 0, 167, 53);
-		frameCrearFichaVehiculo.getContentPane().add(mnMenu);
-		mnMenu.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495928384_dot-more-menu-hide.png")));
-		
-		JMenu mnCoches = new JMenu("");
-		mnCoches.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495927937_solid-travel-tourism-car.png")));
+
+		//MENU
+		menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 157, 48);
+		frameCrearFichaVehiculo.getContentPane().add(menuBar);
+
+		menuBar.add(mnMenu);
+		mnMenu.setIcon(
+				new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495928384_dot-more-menu-hide.png")));
+
+		mnCoches.setIcon(
+				new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495927937_solid-travel-tourism-car.png")));
 		mnMenu.add(mnCoches);
-		
-		JMenu mnClientes = new JMenu("");
+
+		mntmCocheLeer.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930663_67.png")));
+		mnCoches.add(mntmCocheLeer);
+
+		mntmCocheEditar.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930865_pen.png")));
+		mnCoches.add(mntmCocheEditar);
+
+		mntmCocheCrear
+				.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495931018_new_product.png")));
+		mnCoches.add(mntmCocheCrear);
+
 		mnClientes.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495929007_104.png")));
 		mnMenu.add(mnClientes);
-		
+
+		mntmClienteLeer.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930663_67.png")));
+		mnClientes.add(mntmClienteLeer);
+
+		mntmClienteEditar.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930865_pen.png")));
+		mnClientes.add(mntmClienteEditar);
+
+		mntmClienteCrear
+				.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495931018_new_product.png")));
+		mnClientes.add(mntmClienteCrear);
+
 		mnReparaciones = new JMenu("");
 		mnReparaciones.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495928663_wrench.png")));
 		mnMenu.add(mnReparaciones);
+
+		mntmReparacionLeer.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930663_67.png")));
+		mnReparaciones.add(mntmReparacionLeer);
+
+		mntmReparacionEditar.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930865_pen.png")));
+		mnReparaciones.add(mntmReparacionEditar);
+
+		mntmReparacionCrear
+				.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495931018_new_product.png")));
+		mnReparaciones.add(mntmReparacionCrear);
 
 	}
 
