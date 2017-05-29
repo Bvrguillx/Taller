6ef4e9ab4e0555c4cb8efe236f8ab9c638e60f5d
Triboga <Triboga@192.168.0.105>
2017-05-29 20:53:46 +0200

    menu en reparar

--- src/taller/Main.java
+++ src/taller/Main.java
@@ -16,8 +16,8 @@ public class Main {
 //	Vehiculo v1 = new Vehiculo("1234ABC", 11, "aa", "aa", "aa", 11, "77777777l");
 //	Vehiculo v2 = new Vehiculo("1233abc", 22, "bb", "bb", "bb", 22, "66666666l");
 //	
-//	Reparacion r1 = new Reparacion("Demo", "1234ABC", "77777777l", null, null, "ent", "111", "aaaaa");
-//	Reparacion r2 = new Reparacion("Demo", "1233abc", "66666666l", null, null, "pen", "222", "bbbbb");
+//	Reparacion r1 = new Reparacion("Demo", "1234ABC", "77777777l", null, null, 1, "111", "aaaaa");
+//	Reparacion r2 = new Reparacion("Demo", "1233abc", "66666666l", null, null, 2, "222", "bbbbb");
 //	
 //	Listas.listaClientes.add(c1);
 //	Listas.listaClientes.add(c2);
@@ -27,7 +27,7 @@ public class Main {
 //	
 //	Listas.listaReparaciones.add(r1);
 //	Listas.listaReparaciones.add(r2);	
-	
+//	
 	login g = new login();
 	g.run();
 
--- src/ventanas/FichaReparar.java
+++ src/ventanas/FichaReparar.java
@@ -9,6 +9,9 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -55,6 +58,22 @@ public class FichaReparar {
 	private JTextField txtCliente;
 	private JLabel lblCliente;
 	private int indiceLista;
+
+	// menu
+	private JMenu mnMenu;
+	private JMenu mnCoches;
+	private JMenuItem mntmCocheLeer;
+	private JMenuItem mntmCocheEditar;
+	private JMenuItem mntmCocheCrear;
+	private JMenu mnClientes;
+	private JMenuItem mntmClienteLeer;
+	private JMenuItem mntmClienteCrear;
+	private JMenuItem mntmReparacionCrear;
+	private JMenuItem mntmReparacionEditar;
+	private JMenuItem mntmReparacionLeer;
+	private JMenuItem mntmClienteEditar;
+	private JMenu mnReparaciones;
+	private JMenuBar menuBar;
 
 	/**
 	 * Launch the application.
@@ -121,6 +140,20 @@ public class FichaReparar {
 		btnPrincipal = new JButton("");
 		lblCliente = new JLabel("Cliente");
 		txtCliente = new JTextField();
+		// MENU
+		mnMenu = new JMenu("");
+		mnCoches = new JMenu("");
+		mntmCocheLeer = new JMenuItem("");
+		mntmCocheEditar = new JMenuItem("");
+		mntmCocheCrear = new JMenuItem("");
+		mnClientes = new JMenu("");
+		mntmClienteLeer = new JMenuItem("");
+		mntmClienteCrear = new JMenuItem("");
+		mntmClienteEditar = new JMenuItem("");
+		mnReparaciones = new JMenu("");
+		mntmReparacionCrear = new JMenuItem("");
+		mntmReparacionEditar = new JMenuItem("");
+		mntmReparacionLeer = new JMenuItem("");
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes", "serial" })
@@ -133,7 +166,7 @@ public class FichaReparar {
 
 		lblClientesicosDelTrvpller.setFont(new Font("Franklin Gothic Heavy", Font.PLAIN, 20));
 		lblClientesicosDelTrvpller.setHorizontalAlignment(SwingConstants.CENTER);
-		lblClientesicosDelTrvpller.setBounds(6, 0, 707, 46);
+		lblClientesicosDelTrvpller.setBounds(198, 0, 305, 46);
 		frameFichaReparar.getContentPane().add(lblClientesicosDelTrvpller);
 		frameFichaReparar.setResizable(false);
 
@@ -290,6 +323,55 @@ public class FichaReparar {
 		frameFichaReparar.getContentPane().add(txtCliente);
 		txtCliente.setColumns(10);
 		txtCliente.setText(Listas.listaReparaciones.get(Listas.listaReparaciones.size() - 1).getDniDuenio());
+		
+		menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 157, 48);
+		//menu
+		frameFichaReparar.getContentPane().add(menuBar);
+
+		menuBar.add(mnMenu);
+		mnMenu.setIcon(
+				new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495928384_dot-more-menu-hide.png")));
+
+		mnCoches.setIcon(
+				new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495927937_solid-travel-tourism-car.png")));
+		mnMenu.add(mnCoches);
+
+		mntmCocheLeer.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930663_67.png")));
+		mnCoches.add(mntmCocheLeer);
+
+		mntmCocheEditar.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930865_pen.png")));
+		mnCoches.add(mntmCocheEditar);
+
+		mntmCocheCrear
+				.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495931018_new_product.png")));
+		mnCoches.add(mntmCocheCrear);
+
+		mnClientes.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495929007_104.png")));
+		mnMenu.add(mnClientes);
+
+		mntmClienteLeer.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930663_67.png")));
+		mnClientes.add(mntmClienteLeer);
+
+		mntmClienteEditar.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930865_pen.png")));
+		mnClientes.add(mntmClienteEditar);
+
+		mntmClienteCrear
+				.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495931018_new_product.png")));
+		mnClientes.add(mntmClienteCrear);
+
+		mnReparaciones.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495928663_wrench.png")));
+		mnMenu.add(mnReparaciones);
+
+		mntmReparacionLeer.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930663_67.png")));
+		mnReparaciones.add(mntmReparacionLeer);
+
+		mntmReparacionEditar.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495930865_pen.png")));
+		mnReparaciones.add(mntmReparacionEditar);
+
+		mntmReparacionCrear
+				.setIcon(new ImageIcon(FichaVehiculoCrear.class.getResource("/iconos/1495931018_new_product.png")));
+		mnReparaciones.add(mntmReparacionCrear);
 	}
 
 	public void setEventos() {
@@ -408,22 +490,22 @@ public class FichaReparar {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 
-				indiceLista=Listas.listaReparaciones.size()-1;
-				
-				Date fechaEntrada=fechaEntradaDC.getDate();
-				Date fechaSalida=fechaSalidaDC.getDate();
-				int Estado= listEstado.getSelectedIndex();
+				indiceLista = Listas.listaReparaciones.size() - 1;
+
+				Date fechaEntrada = fechaEntradaDC.getDate();
+				Date fechaSalida = fechaSalidaDC.getDate();
+				int Estado = listEstado.getSelectedIndex();
 				String presupuesto = presupuestoField.toString();
 				String comentarios = comentariosArea.getText();
-				
+
 				Listas.listaReparaciones.get(indiceLista).setFechaEntrada(fechaEntrada);
 				Listas.listaReparaciones.get(indiceLista).setFechaSalida(fechaSalida);
 				Listas.listaReparaciones.get(indiceLista).setListEstado(Estado);
 				Listas.listaReparaciones.get(indiceLista).setPresupuestoField(presupuesto);
 				Listas.listaReparaciones.get(indiceLista).setComentariosArea(comentarios);
-				
+
 				frameFichaReparar.dispose();
-				Principal p =new Principal();
+				Principal p = new Principal();
 				p.getFramePrincipal().setVisible(true);
 			}
 		});
