0922b49dcac548f50c863b795bf1f1c33297be2a
admincenec <admincenec@AULA519.cenecmalaga.net>
2017-05-30 12:21:02 +0200

    Descomentado el main

--- src/taller/Main.java
+++ src/taller/Main.java
@@ -10,24 +10,25 @@ public class Main {
 	
 	public static void main(String[] args){
 	
-//	Cliente c1 = new Cliente("1", 1, "1", "77777777l", 1);
-//	Cliente c2 = new Cliente("2", 2, "2", "66666666l", 2);
-//	
-//	Vehiculo v1 = new Vehiculo("1234ABC", 11, "aa", "aa", "aa", 11, "77777777l");
-//	Vehiculo v2 = new Vehiculo("1233abc", 22, "bb", "bb", "bb", 22, "66666666l");
-//	
-//	Reparacion r1 = new Reparacion("Demo", "1234ABC", "77777777l", null, null, 1, "111", "aaaaa");
-//	Reparacion r2 = new Reparacion("Demo", "1233abc", "66666666l", null, null, 2, "222", "bbbbb");
-//	
-//	Listas.listaClientes.add(c1);
-//	Listas.listaClientes.add(c2);
-//	
-//	Listas.listaVehiculo.add(v1);
-//	Listas.listaVehiculo.add(v2);
-//	
-//	Listas.listaReparaciones.add(r1);
-//	Listas.listaReparaciones.add(r2);	
-//	
+	Cliente c1 = new Cliente("1", 1, "1", "77777777l", 1);
+	Cliente c2 = new Cliente("2", 2, "2", "66666666l", 2);
+	
+	Vehiculo v1 = new Vehiculo("1234ABC", 11, "aa", "aa", "aa", 11, "77777777l");
+	Vehiculo v2 = new Vehiculo("1233abc", 22, "bb", "bb", "bb", 22, "66666666l");
+	
+	Reparacion r1 = new Reparacion("Demo", "1234ABC", "77777777l", null, null, 1, "111", "aaaaa");
+	Reparacion r2 = new Reparacion("Demo", "1233abc", "66666666l", null, null, 2, "222", "bbbbb");
+	Listas.usuario="";
+	
+	Listas.listaClientes.add(c1);
+	Listas.listaClientes.add(c2);
+	
+	Listas.listaVehiculo.add(v1);
+	Listas.listaVehiculo.add(v2);
+	
+	Listas.listaReparaciones.add(r1);
+	Listas.listaReparaciones.add(r2);	
+	
 	login g = new login();
 	g.run();
 
